093e4ca5ca62fc40aef041eb1140ce1546d27289
Charles Carbonel <carbonel257@gmail.com>
2023-10-20 20:11:13 -0500

    Added ability to interact with shell

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@
 use dioxus::prelude::*;
 use nix::pty::forkpty;
 use nix::unistd::{read, ForkResult};
+use regex::Regex;
+// use core::panicking::panic;
 use std::env;
 use std::fs::File;
+use std::io::{Error, Write};
 use std::os::fd::IntoRawFd;
 use std::os::unix::io::{FromRawFd, RawFd};
 use std::process::{Command, Stdio};
@@ -17,6 +20,18 @@ fn read_from_fd(fd: RawFd) -> Option<Vec<u8>> {
         Err(_e) => None,
     }
 }
+
+fn remove_ansi_escape_codes(input: &str) -> String {
+    // Regular expression pattern to match ANSI escape codes
+    let ansi_escape_code_regex = Regex::new(r"\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]").unwrap();
+
+    // Replace ANSI escape codes with an empty string
+    ansi_escape_code_regex
+        .replace_all(input, "")
+        .to_string()
+        .replace("bash-3.2$", "")
+}
+
 fn spawn_pty_with_shell(default_shell: String) -> RawFd {
     match unsafe { forkpty(None, None) } {
         Ok(fork_pty_res) => {
@@ -36,14 +51,39 @@ fn spawn_pty_with_shell(default_shell: String) -> RawFd {
     }
 }
 
+fn user_command(&stdout_fd: &i32, input: &str) -> String {
+    let output_file: File = unsafe { File::from_raw_fd(stdout_fd) };
+    let mut output = output_file.try_clone().expect("Unable to clone");
+
+    if let Err(e) = write!(output, "{} \n", input) {
+        panic!("There was an error writing the output: {:?}", e)
+    }
+    match output.flush() {
+        Ok(_) => (),
+        Err(_) => panic!("There was an error flushing the output!"),
+    }
+    loop {
+        match read_from_fd(stdout_fd) {
+            Some(read_bytes) => {
+                let std_out: String = String::from_utf8(read_bytes).unwrap();
+                return remove_ansi_escape_codes(&std_out);
+            }
+            None => continue,
+        }
+    }
+}
+
 fn App(cx: Scope) -> Element {
+    let default_shell = std::env::var("SHELL").expect("Unable to load shell");
+    let stdout_fd = spawn_pty_with_shell(default_shell);
+
     let user_input = use_state(cx, || "".to_string());
     let command = use_state(cx, || "".to_string());
 
     let handle_input_submit = move |event: KeyboardEvent| {
         if event.key().to_string() == "Enter" {
-            println!("Enter was pressed");
-            command.set(user_input.to_string());
+            let response = user_command(&stdout_fd, user_input);
+            command.set(response);
             user_input.set("".to_string());
         };
     };
@@ -64,7 +104,8 @@ fn App(cx: Scope) -> Element {
                 padding: "10px",
                 p {
                     if command.chars().count() > 0 {
-                    "Why don't you just Google the answer?"
+                    // "Why don't you just Google the answer?"
+                    command
                     } else {
                         ""
                     }
@@ -105,6 +146,9 @@ fn App(cx: Scope) -> Element {
 }
 
 fn main() {
+    // let shell = std::env::var("SHELL").expect("Unable to load shell");
+    // let pty = spawn_pty_with_shell(shell);
+    // let output_fd: File = unsafe { File::from_raw_fd(pty) };
     dioxus_desktop::launch_cfg(
         App,
         dioxus_desktop::Config::new()
